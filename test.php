249bb1136578bce0c180900fa52cbf7f7cd13414
Alexys Leclerc <alexys.leclerc@gmail.com>
2017-07-13 09:47:50 -0400

    Update test.php

--- test.php
+++ test.php
@@ -112,3 +112,36 @@ $bd->insertFromArray($testArrayWithNoId)->execute()->getResult();
                         )
  *
  */
+
+
+$testArray2 = ["name" => ["Bob"],
+    "description" => [1]
+];
+$testArrayReverse = [["name" => "Bob", "description" => 6],
+    ["name" => "Jacques", "description" => 7],
+    ["name" => "Julien", "description" => 8],
+    ["name" => "David", "description" => 9],
+    ["name" => "Pablo", "description" => 10]
+];
+
+$testArrayReverse2 = ["name" => "Bob", "description" => 6];
+
+var_dump(is_array(reset($testArray2)));  //true
+var_dump(is_array(reset($testArrayReverse)));  //true
+var_dump(is_array(reset($testArrayReverse2)));  //false
+var_dump(is_array(false));  //false
+
+
+var_dump(test($testArray2));    //true
+var_dump(test($testArrayReverse));  //true
+var_dump(test($testArrayReverse2)); //false
+var_dump(test(false));  //false
+
+function test($array) {
+    foreach ($array as $subArray) {
+        if (is_array($subArray)) {
+            return true;
+        }
+    }
+    return false;
+}
